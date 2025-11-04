9af5ed5a7d7bd00941433f94b7ee8a244318245e
Benedict Ecker <b.ecker@bitteeinbyte.de>
2025-11-04 13:28:20 +0100

    Added Setup-Script for all the Pro-Audio tools I usually use.

---
+++ Setups/Setup_ProAudio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get Operating System Name
+OS=$(awk -F= '/^NAME=/ {print $2}' /etc/os-release | tr -d '"')
+
+# CachyOS
+if [[ "$OS" == "CachyOS Linux" ]]
+then
+    sudo pacman -Sy --noconfirm pro-audio alsa-scarlett-gui bitwig-studio reapack yabridgectl
+fi

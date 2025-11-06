0b37030c59baed4f78094dffaf7e34e090582fe4
Benedict Ecker <b.ecker@bitteeinbyte.de>
2025-11-06 15:53:52 +0100

    Some more tools I use.

--- Setups/Setup_Tools_Shell.sh
+++ Setups/Setup_Tools_Shell.sh
@@ -6,10 +6,10 @@ OS=$(awk -F= '/^NAME=/ {print $2}' /etc/os-release | tr -d '"')
 # Arch Linux
 if [[ "$OS" == "Arch Linux" || "$OS" == "CachyOS Linux" ]]
 then
-    sudo pacman -Sy --noconfirm acl at bash bash-completion bat bind-tools btop cmatrix cowsay curl ddrescue diffutils exfatprogs fastfetch findutils fio fzf gnupg grep gzip hdparm htop iftop iperf3 iputils libarchive lm_sensors lolcat lsof lsscsi lz4 mbuffer mc mtr ncdu netcat nvme-cli openssl p7zip parallel perl-rename powertop rsnapshot rsync sl smartmontools stress tar tmux traceroute tree udftools unrar wget wipe xxhash yq zip zsh zstd
+    sudo pacman -Sy --noconfirm acl at bash bash-completion bat bind-tools btop cmatrix cowsay curl ddrescue diffutils exfatprogs fastfetch findutils fio fortune-mod fzf gnupg grep gzip hdparm htop iftop iperf3 iputils libarchive lm_sensors lolcat lsof lsscsi lz4 mbuffer mc mtr ncdu netcat nvme-cli openssl p7zip parallel perl-rename powertop rsnapshot rsync sl smartmontools stress tar tmux traceroute tree udftools unrar wget wipe xxhash yq zip zsh zstd
 
 # Fedora
 elif [[ "$OS" == "Fedora Linux" ]]
 then
-    sudo dnf install -y acl at bash bash-completion bat bind-utils btop cmatrix cowsay curl ddrescue diffutils exfatprogs fastfetch findutils fio fzf gnupg grep gzip hdparm htop iftop iperf3 iputils libarchive lm_sensors lolcat lsof lsscsi lz4 mbuffer mc mtr ncdu netcat nvme-cli openssl p7zip parallel prename powertop rsnapshot rsync sl smartmontools stress tar tmux traceroute tree udftools unrar wget wipe xxhash yq zip zsh zstd
+    sudo dnf install -y acl at bash bash-completion bat bind-utils btop cmatrix cowsay curl ddrescue diffutils exfatprogs fastfetch findutils fio fortune-mod fzf gnupg grep gzip hdparm htop iftop iperf3 iputils libarchive lm_sensors lolcat lsof lsscsi lz4 mbuffer mc mtr ncdu netcat nvme-cli openssl p7zip parallel prename powertop rsnapshot rsync sl smartmontools stress tar tmux traceroute tree udftools unrar wget wipe xxhash yq zip zsh zstd
 fi

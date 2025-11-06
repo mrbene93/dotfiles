824197b03511ae6e927580d92670bc9813f4c977
Benedict Ecker <b.ecker@bitteeinbyte.de>
2025-11-06 15:26:58 +0100

    Choose SDDM as display manager for my future Wayland-based GUI Setup and added catppuccin themes and config files.

--- Setups/Setup_Hyprland.sh
+++ Setups/Setup_Hyprland.sh
@@ -6,5 +6,6 @@ OS=$(awk -F= '/^NAME=/ {print $2}' /etc/os-release | tr -d '"')
 # CachyOS
 if [[ "$OS" == "CachyOS Linux" ]]
 then
-    sudo pacman -Sy --noconfirm fuzzel hyprcursor hypridle hyprland hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprsunset nwg-displays swaync waybar xdg-desktop-portal-hyprland
+    sudo pacman -Sy --noconfirm fuzzel hyprcursor hypridle hyprland hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprsunset noto-fonts nwg-displays pipewire pipewire-alsa pipewire-audio pipewire-docs pipewire-jack pipewire-pulse qt5-quickcontrols2 qt6-svg qt6-declarative sddm swaync waybar wiremix wireplumber wireplumber-docs xdg-desktop-portal-hyprland
 fi
+

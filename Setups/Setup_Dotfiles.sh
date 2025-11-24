7efd02cb06f7f2769cdc64a240e27f3099c74c2e
Benedict Ecker <b.ecker@bitteeinbyte.de>
2025-11-24 11:49:45 +0100

    Setup_Dotfiles overwork number 2 :)

--- Setups/Setup_Dotfiles.sh
+++ Setups/Setup_Dotfiles.sh
@@ -1,42 +1,15 @@
 #!/bin/bash
 
 
-# Get Operating System Name
-OS=$(awk -F= '/^NAME=/ {print $2}' /etc/os-release | tr -d '"')
-
-# Ensure required packages are installed
-if [[ "$OS" == "Arch Linux" || "$OS" == "CachyOS Linux" ]]
-then
-    sudo pacman -Sy --noconfirm curl git openssh
-elif [[ "$OS" == "Fedora Linux" ]]
-then
-    sudo dnf install --assumeyes curl git openssh
-fi
-
 # Variables
-dotdir="$HOME/.git/dotfiles"
-usershell='zsh'
-
-# Setup SSH
-if [[ ! -s "$HOME/.ssh/id_ed25519_ecker-privat" ]]
-then
-    echo "Private SSH-Key not found. Please configure SSH correctly and restart the script."
-    exit 1
-fi
-if [[ ! -s "$HOME/.ssh/config" ]]
-then
-    curl -o "$HOME/.ssh/config" "https://raw.githubusercontent.com/mrbene93/dotfiles/refs/heads/master/.ssh/config"
-fi
-
-# Pull dotfiles repo
-if [[ ! -d $dotdir ]]
-then
-    git clone git@github.com:mrbene93/dotfiles.git $dotdir
-    git -C $dotdir submodule update --init --recursive $dotdir
-fi
-
-# Create directories
-directories=(
+dotdir="$HOME/.git/dotfiles"    # Directory which should contain my dotfiles git repository
+usershell='zsh'                 # Which shell to set as default
+packages=(                      # Required packages to run the script
+    "curl"
+    "git"
+    "openssh"
+)
+directories=(                   # Directories to create
     ".config"
     ".config/alacritty"
     ".config/btop"
@@ -48,14 +21,7 @@ directories=(
     ".local/share/icons"
     ".vim"
 )
-for directory in ${directories[@]}
-do
-    mkdir -p "$HOME/$directory"
-done
-
-
-# Create symlinks
-symlinks=(
+symlinks=(                      # Symlinks to create
     ".abcde.conf"
     ".fluxbox"
     ".gitconfig"
@@ -82,6 +48,46 @@ symlinks=(
     ".local/share/icons/Bibata-Modern-Ice"
     "Wallpaper"
 )
+
+
+# Get Operating System Name
+OS=$(awk -F= '/^NAME=/ {print $2}' /etc/os-release | tr -d '"')
+
+# Ensure required packages are installed
+if [[ "$OS" == "Arch Linux" || "$OS" == "CachyOS Linux" ]]
+then
+    sudo pacman -Sy --needed --noconfirm ${packages[@]}
+elif [[ "$OS" == "Fedora Linux" ]]
+then
+    sudo dnf install --assumeyes ${packages[@]}
+fi
+
+# Setup SSH
+if [[ ! -s "$HOME/.ssh/id_ed25519_ecker-privat" ]]
+then
+    echo "Private SSH-Key not found. Please configure SSH correctly and restart the script."
+    exit 1
+fi
+if [[ ! -s "$HOME/.ssh/config" ]]
+then
+    curl -o "$HOME/.ssh/config" "https://raw.githubusercontent.com/mrbene93/dotfiles/refs/heads/master/.ssh/config"
+fi
+
+# Pull dotfiles repo
+if [[ ! -d $dotdir ]]
+then
+    git clone git@github.com:mrbene93/dotfiles.git $dotdir
+    git -C $dotdir submodule update --init --recursive $dotdir
+fi
+
+# Create directories
+for directory in ${directories[@]}
+do
+    mkdir -p "$HOME/$directory"
+done
+
+
+# Create symlinks
 for symlink in ${symlinks[@]}
 do
     ln -sf "$dotdir/$symlink" "$HOME/$symlink"

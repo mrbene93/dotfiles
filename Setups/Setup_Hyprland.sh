3aa959aa6308d156f6ba23a04f7daa6d0d3a0403
Benedict Ecker <b.ecker@bitteeinbyte.de>
2025-11-24 13:57:27 +0100

    Hyprland Setup-Script: Replaced pipewire-jack with pipewire-jack-client.

--- Setups/Setup_Hyprland.sh
+++ Setups/Setup_Hyprland.sh
@@ -6,6 +6,6 @@ OS=$(awk -F= '/^NAME=/ {print $2}' /etc/os-release | tr -d '"')
 # CachyOS
 if [[ "$OS" == "CachyOS Linux" ]]
 then
-    sudo pacman -Sy --needed --noconfirm fuzzel hyprcursor hypridle hyprland hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprsunset noto-fonts nwg-displays pipewire pipewire-alsa pipewire-audio pipewire-docs pipewire-jack pipewire-pulse qt5-quickcontrols2 qt6-svg qt6-declarative sddm swaync waybar wiremix wireplumber wireplumber-docs wlogout xdg-desktop-portal-hyprland
+    sudo pacman -Sy --needed --noconfirm fuzzel hyprcursor hypridle hyprland hyprlock hyprpaper hyprpicker hyprpolkitagent hyprshot hyprsunset noto-fonts nwg-displays pipewire pipewire-alsa pipewire-audio pipewire-docs pipewire-jack-client pipewire-pulse qt5-quickcontrols2 qt6-svg qt6-declarative sddm swaync waybar wiremix wireplumber wireplumber-docs wlogout xdg-desktop-portal-hyprland
 fi
 
